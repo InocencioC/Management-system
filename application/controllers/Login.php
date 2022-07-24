0c75f0362ce2a58303e47d0fd5feed2baa2ac9be
InocencioC <cordeiroturorias@gmail.com>
2022-07-24 21:32:40 +0100

    now its possible make a logout, with method logout

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,24 +6,33 @@ class Login extends CI_Controller {
 
     public function index() {
 
-     /* 
-    [email] => canalti@gmail.com
-    [password] => 1234
-*/
-
-$identity = $this->security->xss_clean($this->input->post('email'));
-$password =  $this->security->xss_clean($this->input->post('password'));
-$remember = FALSE; // remember the user
-if($this->ion_auth->login($identity, $password, $remember)) {
-    
-    redirect('home');
-} else {
-
-    $this->session->set_flashdata('error', 'Verifique o seu e-mail ou senha');
-        
+$data = array(
+    'titulo' => 'Login',
+);
         $this->load->view('layout/header');
         $this->load->view('Login/index');
         $this->load->view('layout/footer');
     }
-  }
+
+public function auth() {
+
+$identity = $this->security->xss_clean($this->input->post('email'));
+$password =  $this->security->xss_clean($this->input->post('password'));
+$remember = FALSE; // remember the user
+
+if($this->ion_auth->login($identity, $password, $remember)) {
+   redirect('home');
+} else {
+
+   $this->session->set_flashdata('error', 'Verifique o seu e-mail ou senha');
+   redirect('login');  
+  
+   }
+ }
+
+ public function logout() {
+    $this->ion_auth->logout();
+    redirect('login');
+ }
+
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -31,7 +31,7 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Seja bem vindo!</h1>
                   </div>
-                  <form class="user" name="form_index" method="POST">
+                  <form class="user" name="form_auth" method="POST" action="<?php echo base_url('login/auth'); ?>">
                     <div class="form-group">
                       <input type="email" name="email" class="form-control form-control-user"  placeholder="Entre com o seu e-mail...">
                     </div>
