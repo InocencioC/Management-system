d6f3d17b874a8f4f34cd49f274174f91b44e1b60
InocencioC <cordeiroturorias@gmail.com>
2022-07-25 21:35:55 +0100

    working in module system

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -5,6 +5,12 @@ defined('BASEPATH') OR exit ('Ação não permitida');
 class Usuarios extends CI_Controller {
     public function __construct() {
         parent::__construct();
+
+        if (!$this->ion_auth->logged_in()) {
+            $this->session->set_flashdata('info', 'Sua sessão expirou!');
+            redirect('login');
+    
+        }   
         
         //Definide if have a session
     }
